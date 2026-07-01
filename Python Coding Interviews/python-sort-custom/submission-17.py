5604674715d2709340ad9f9bd1d17a7334b8f5e7
ncham mancha <manchancham@gmail.com>
2026-07-01 00:04:47 -0700

    Add: python-sort-custom - submission-17

---
+++ Python Coding Interviews/python-sort-custom/submission-17.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    t=[]
+    for i in words:
+        largest=len(words[0])
+        if len(i)>largest:
+            largest=i
+            words.remove(largest)
+            t.append(largest)
+    return t
+    
+
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    t=[]
+
+    for i in numbers:
+        t.append(abs(i))
+        l=sorted(t)
+    return l
+    
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
