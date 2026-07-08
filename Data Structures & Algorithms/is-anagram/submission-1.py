b107cca679d459c302a9eff3eefb8e7a51024a9f
ncham mancha <manchancham@gmail.com>
2026-07-08 03:07:56 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        m=[]
+        for letter in s:
+            if letter in t:
+                m.append(letter)
+                if len(m)==len(s) and len(m)==len(s):
+                    return True 
+            else:
+                return False 
+        
