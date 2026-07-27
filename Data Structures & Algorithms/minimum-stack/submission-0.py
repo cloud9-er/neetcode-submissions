8697291161bbe2150c5cf6db8f7c3333fd84af9b
ncham mancha <manchancham@gmail.com>
2026-07-27 22:02:49 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.minstack=[]
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minstack or val<=self.minstack[-1]:
+            self.minstack.append(val)
+        return None
+
+        
+
+    def pop(self) -> None:
+        value=self.stack.pop(-1)
+        if value==self.minstack[-1]:
+            self.minstack.pop()
+
+
+        return None
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+        
