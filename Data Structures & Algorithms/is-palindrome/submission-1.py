097c70ad0e57cccb9c3319e6eb7589c721c45a11
ncham mancha <manchancham@gmail.com>
2026-08-10 18:38:00 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res=''
+        s=s.lower()
+        for i in s:
+            if i.isalnum():
+                res+=i
+        return res==res[::-1] 
+            
