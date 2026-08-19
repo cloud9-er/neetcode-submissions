011bc37ac9a8d9f8fb27f7353a8726e10aad051b
ncham mancha <manchancham@gmail.com>
2026-08-19 19:14:27 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l=len(nums)
+        for i in range(l):
+            for j in range(l):
+                if nums[j] + nums[i] == target and nums[j] !=nums[i]:
+                    smallest=min(i,j)
+                    largest=max(i,j)
+            return [smallest,largest]
+
+
+       
